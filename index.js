6574d54c05abcd89f757533934115828397b6406
Kassiohenrique90 <kassiohenrique8871@gmail.com>
2024-10-23 22:26:45 -0300

    Desafio concluido

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
 
 
-console.log("Olá pessoal")
+let nomeHeroi = "SupremoMix"
+let nivel = 10000
+
+if(nivel <= 1000)
+{
+    console.log("Ferro")
+    nivel = "Ferro"
+}
+else if(nivel >= 1001 && nivel <= 2000)
+{
+    console.log("Bronze")
+    nivel = "Bronze"
+}
+else if(nivel >= 2001 && nivel <= 5000)
+{
+    console.log("Prata")
+    nivel = "Prata"
+}
+else if(nivel >= 5001 && nivel <= 7000)
+{
+     console.log("Ouro")
+     nivel = "Ouro"
+}
+else if(nivel >= 7001 && nivel <= 8000)
+{
+     console.log("Platina")
+     nivel = "Platina"
+}
+else if(nivel >= 8001 && nivel <= 9000)
+{
+     console.log("Ascendente")
+     nivel = "Ascendente"
+}
+else if(nivel >= 9001 && nivel <= 10000)
+{
+        console.log("Imortal")
+        nivel = "Imortal"
+}
+else
+{
+    console.log("Radiante")
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível " + nivel)
